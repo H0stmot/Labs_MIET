b50422a4a760318138132d6be0e591cd09bbd844
H0stmot <romarybakov470@gmail.com>
2026-04-22 14:19:50 +0300

    LSU integration into processor_system.sv

--- Datapath07/processor_system.sv
+++ Datapath07/processor_system.sv
@@ -5,17 +5,27 @@ module processor_system(
 
   logic [31:0] instr_addr;
   logic [31:0] instr;
+  
+  
+  logic        core_req;
+  logic        core_we;
+  logic [2:0]  core_size;
+  logic [31:0] core_addr;
+  logic [31:0] core_wd;
+  logic [31:0] core_rd;
+  logic        stall;
 
   
   logic        mem_req;
   logic        mem_we;
-  logic [2:0]  mem_size; 
+  logic [3:0]  mem_be;
   logic [31:0] mem_addr;
   logic [31:0] mem_wd;
   logic [31:0] mem_rd;
+  logic        mem_ready;
 
-
-  logic stall;
+//логика stall теперь находиться в LSU
+ /* logic stall;
   logic stall_next;
 
   assign stall_next = mem_req & (~stall);
@@ -26,37 +36,59 @@ module processor_system(
     end else begin
       stall <= stall_next;
     end
-  end
+  end*/
 
-  processor_core core (
-    .clk_i        (clk_i),
-    .rst_i        (rst_i),
-    .stall_i      (stall),      
-    .instr_addr_o (instr_addr), 
-    .instr_i      (instr),      
-    .mem_rd_i     (mem_rd),     
-    .mem_addr_o   (mem_addr),   
-    .mem_size_o   (mem_size),   
-    .mem_req_o    (mem_req),    
-    .mem_we_o     (mem_we),    
-    .mem_wd_o     (mem_wd)      
-  );
+ processor_core core (
+   .clk_i        (clk_i),
+   .rst_i        (rst_i),
+   .stall_i      (stall),
+   .instr_addr_o (instr_addr),
+   .instr_i      (instr),
+   .mem_rd_i     (core_rd),      
+   .mem_addr_o   (core_addr),   
+   .mem_size_o   (core_size),  
+   .mem_req_o    (core_req),     
+   .mem_we_o     (core_we),      
+   .mem_wd_o     (core_wd)      
+ );
 
 
   instr_mem instruction_memory (
     .read_addr_i (instr_addr), 
     .read_data_o (instr)       
   );
-
+  
 
   data_mem data_memory (
-    .clk_i          (clk_i),       
-    .mem_req_i      (mem_req),    
-    .write_enable_i (mem_we),      
-    .byte_enable_i  (4'b1111),     
+    .clk_i          (clk_i),
+    .mem_req_i      (mem_req),     
+    .write_enable_i (mem_we),       
+    .byte_enable_i  (mem_be),      
     .addr_i         (mem_addr),    
-    .write_data_i   (mem_wd),      
-    .read_data_o    (mem_rd),      
-    .ready_o        ()             
+    .write_data_i   (mem_wd),       
+    .read_data_o    (mem_rd),     
+    .ready_o        (mem_ready)     
   );
+  
+  lsu lsu_integration (
+    .clk_i        (clk_i),
+    .rst_i        (rst_i),
+
+    .core_req_i   (core_req),
+    .core_we_i    (core_we),
+    .core_size_i  (core_size),
+    .core_addr_i  (core_addr),
+    .core_wd_i    (core_wd),
+    .core_rd_o    (core_rd),
+    .core_stall_o (stall),
+
+    .mem_req_o    (mem_req),
+    .mem_we_o     (mem_we),
+    .mem_be_o     (mem_be),
+    .mem_addr_o   (mem_addr),
+    .mem_wd_o     (mem_wd),
+    .mem_rd_i     (mem_rd),
+    .mem_ready_i  (mem_ready)
+    );
+    
 endmodule
